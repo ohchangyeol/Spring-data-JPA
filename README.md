0a0e7bcc810e6dd0cc9e5b37491e6a2fdb8b5bfa
ohcnagyeol <cy950315@naver.com>
2022-06-08 01:13:12 +0900

    test & yml,build setting.
    - MemberRepositoryTest.java
    - build.gradle
    - application.yml
    - README.md

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 
 김영한 개발자님의 [인프런 강의](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-JPA-%ED%99%9C%EC%9A%A9-1)를 보고 학습한 내용입니다.
 
-### 현재 Repository의 내용은
+#### 현재 Repository의 내용은
 - JPA 활용1 (JPA_01)
 - JPA 활용2 (JPA_02)
 - 스프링데이터 JPA (SD_JPA)
 - QueryDSL (DSL)
 
 순으로 강의를 보며 따라할 계획이며 branch로 버전 관리를 할 예정입니다.
+
+## JPA 활용1 
+*Branch Name :: JPA_01*
+
+- JPA의 동작하는 모든 Entity는 transaction안에서 이루어져야만 한다.
+- Test code에서 이루어지는 데이터는 rollback
+- rollback(value = false) 함으로서 rollback을 취소한다.
+- 쿼리 파라미터 로그 남기는 설정  
+  application.yml -> ```org.hibernate.type: trace```  
+  build.gradle -> ```com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.5.6```  
+  **다만 배포시엔 성능 이슈가 될수 있기 때문에 개발단계에서만 쓴는 것을 권장**
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,12 +1,14 @@
 package jpabook.jpashop;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -15,12 +17,22 @@ class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(value = false)
+    // JPA는 모든 Entity는 트랜잭션 안에서 이루어져야 한다.
     public void testMember() throws Exception{
         //given
+        Member member = new Member();
+        member.setUserName("memberA");
 
         //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
 
         //then
-
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        // 같은 영속성 컨텍스트 안에 있기 때문에 같은 ID를 가지고있기 때문에 같은 객체라고 인식한다.
     }
 }
